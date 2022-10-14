2f2bc09b919833408691f37cf11dcda0c1546649
felipeperaltacheung <f33___@hotmail.com>
2022-10-14 07:48:52 -0300

    Classe util Date

---
+++ devDojoSpring/src/main/java/com/example/devdojospring/util/DateUtil.java
@@ -0,0 +1,12 @@
+package com.example.devdojospring.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtil {
+
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+
+}
