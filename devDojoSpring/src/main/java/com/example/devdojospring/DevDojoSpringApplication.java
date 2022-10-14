5e07d958efbb946bcbce87426b407b2adcabff63
felipeperaltacheung <f33___@hotmail.com>
2022-10-14 07:21:37 -0300

    Project created

---
+++ devDojoSpring/src/main/java/com/example/devdojospring/DevDojoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.devdojospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDojoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevDojoSpringApplication.class, args);
+    }
+
+}
