70a245d2241af3bb3df7e507e0a77d1003bbd191
felipeperaltacheung <f33___@hotmail.com>
2022-10-16 02:47:33 -0300

    Adicionando o ResponseEntity

--- devDojoSpring/src/main/java/com/example/devdojospring/controller/AnimeController.java
+++ devDojoSpring/src/main/java/com/example/devdojospring/controller/AnimeController.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +26,10 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list (){
+    public ResponseEntity<List<Anime>> list (){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        //return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK); ou
+        return ResponseEntity.ok(animeService.listAll());
     }
 
 
