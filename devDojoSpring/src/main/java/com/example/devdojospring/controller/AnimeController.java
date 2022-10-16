02fb09308ede9383e142ee0a639bda003bda8c82
felipeperaltacheung <f33___@hotmail.com>
2022-10-16 04:05:00 -0300

    criando metodo getmapping findById retornando animes.stream e inserindo server error include stack stace no .yml

--- devDojoSpring/src/main/java/com/example/devdojospring/controller/AnimeController.java
+++ devDojoSpring/src/main/java/com/example/devdojospring/controller/AnimeController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,9 @@ public class AnimeController {
         return ResponseEntity.ok(animeService.listAll());
     }
 
-
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
+    }
 
 }
--- devDojoSpring/src/main/java/com/example/devdojospring/service/AnimeService.java
+++ devDojoSpring/src/main/java/com/example/devdojospring/service/AnimeService.java
@@ -2,16 +2,27 @@ package com.example.devdojospring.service;
 
 
 import com.example.devdojospring.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService{
 
+    private List<Anime> animes = List.of(new Anime(1L,"BOKUBOKU"), new Anime(2L, "Berserk"));
+
     //private final AnimeRepository animorepository;
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"BOKUBOKU"), new Anime(2L, "Berserk"));
+        return animes;
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 
 }
