43dce4c202c723875b4200f2b5b44f08c979d27f
felipeperaltacheung <f33___@hotmail.com>
2022-10-16 01:52:11 -0300

    Adicionando lombok e implementando log

--- devDojoSpring/src/main/java/com/example/devdojospring/controller/AnimeController.java
+++ devDojoSpring/src/main/java/com/example/devdojospring/controller/AnimeController.java
@@ -1,19 +1,28 @@
 package com.example.devdojospring.controller;
 
 import com.example.devdojospring.domain.Anime;
+import com.example.devdojospring.util.DateUtil;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
 public class AnimeController {
+
+    @Autowired
+    private DateUtil dateUtil;
 
     //@RequestMapping(method = RequestMethod.GET, path = "list") -> antigo
     @GetMapping(path = "list")
     public List<Anime> list (){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Berserk"));
     }
 
--- devDojoSpring/src/main/java/com/example/devdojospring/util/DateUtil.java
+++ devDojoSpring/src/main/java/com/example/devdojospring/util/DateUtil.java
@@ -1,8 +1,11 @@
 package com.example.devdojospring.util;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class DateUtil {
 
     public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
