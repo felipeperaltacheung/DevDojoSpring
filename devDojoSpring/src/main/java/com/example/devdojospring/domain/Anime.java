48dbf2af56da5746d6adf4ced3bca1e6e5d2ec52
felipeperaltacheung <f33___@hotmail.com>
2022-10-16 23:48:12 -0300

    banco FDP

--- devDojoSpring/src/main/java/com/example/devdojospring/domain/Anime.java
+++ devDojoSpring/src/main/java/com/example/devdojospring/domain/Anime.java
@@ -3,15 +3,21 @@ package com.example.devdojospring.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class Anime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    //@JsonProperty("name") jogar o atributo para name
-    //private String nameCharacter;
-
     private String name;
 }
