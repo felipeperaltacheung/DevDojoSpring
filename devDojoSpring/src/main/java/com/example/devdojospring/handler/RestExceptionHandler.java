2d21131c2027532308de36083d8656d3903a0f65
felipeperaltacheung <f33___@hotmail.com>
2022-10-24 20:48:27 -0300

    Handler

---
+++ devDojoSpring/src/main/java/com/example/devdojospring/handler/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.devdojospring.handler;
+
+import com.example.devdojospring.exception.BadRequestException;
+import com.example.devdojospring.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException){
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("bad request exception, check the documentation")
+                        .details(badRequestException.getLocalizedMessage())
+                        .developerMessage(badRequestException.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+}
