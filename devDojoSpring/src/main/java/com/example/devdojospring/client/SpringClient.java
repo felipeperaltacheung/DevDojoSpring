82bcb3d802a639f6bdefb86beb1310df4df3420b
felipeperaltacheung <f33___@hotmail.com>
2022-11-09 17:49:18 -0300

    placeholder

--- devDojoSpring/src/main/java/com/example/devdojospring/client/SpringClient.java
+++ devDojoSpring/src/main/java/com/example/devdojospring/client/SpringClient.java
@@ -8,11 +8,11 @@ import org.springframework.web.client.RestTemplate;
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
-        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/1", Anime.class);//mapendo automatico com jackson
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);//mapendo automatico com jackson
 
         log.info(entity);
 
-        Anime forObject = new RestTemplate().getForObject("http://localhost:8080/animes/1", Anime.class);
+        Anime forObject = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(forObject);
     }
 }
